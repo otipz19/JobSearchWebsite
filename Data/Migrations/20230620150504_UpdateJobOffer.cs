00e453b4bbdad9abbb48e421731bb152d9e9f327
otipz19 <ot.ipz19@etfkkntu.ukr.education>
2023-06-20 18:05:40 +0300

    Updated JobOffer entity

---
+++ Data/Migrations/20230620150504_UpdateJobOffer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobSearchWebsite.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateJobOffer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "JobOffers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StatusChangedAt",
+                table: "JobOffers",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "JobOffers");
+
+            migrationBuilder.DropColumn(
+                name: "StatusChangedAt",
+                table: "JobOffers");
+        }
+    }
+}
